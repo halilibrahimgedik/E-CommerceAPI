a71bd860c89c762cfc6963d07f37e252366b58d1
halil ibrahim gedik <halilgedik4234@gmail.com>
2024-07-30 15:25:40 +0300

    Add project files.

---
+++ Infrustructure/E-CommerceAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using E_CommerceAPI.Application.Abstractions;
+using E_CommerceAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts() => new()
+        {
+            new(){ Id =  Guid.NewGuid(), Name = "Product 1", Price= 100, Stock = 15 },
+            new(){ Id =  Guid.NewGuid(), Name = "Product 2", Price= 200, Stock = 19 },
+            new(){ Id =  Guid.NewGuid(), Name = "Product 3", Price= 300, Stock = 87 }
+        };
+    }
+}
---
+++ Presentation/E-CommerceAPI.API/Program.cs
@@ -0,0 +1,32 @@
+using E_CommerceAPI.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+
+
+//  1-) Servislerin eklenmesi
+builder.Services.AddPersistenceServices();
+
+
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
